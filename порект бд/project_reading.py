07cdb225bd58f8e9823f8462f27f053c25a99325
ReTup26 <164537603+ReTup26@users.noreply.github.com>
2024-05-19 14:51:13 +0500

    Update project_reading.py

--- порект бд/project_reading.py
+++ порект бд/project_reading.py
@@ -6,52 +6,50 @@ pp = pprint.PrettyPrinter(indent=1, width=80, compact=False)
 ppp = pprint.PrettyPrinter(indent=1, width=80, compact=True)
 c.execute('''PRAGMA foreign_keys = 1''')
 
-"""
+# Пример корректного ввода данных в каждую таблицу
 c.execute("INSERT INTO user (nickname, name, mail, password) VALUES ('turnoff', 'Виктор Тернов', 'turnoff@gmail.com', 'idktexttext')")
 conn.commit()
 
-c.execute("DELETE from user where id_user = 24")
+c.execute("INSERT INTO contact(id_user, id_contact) VALUES (1,4), (4,1)")
+conn.commit()
+
+c.execute("INSERT INTO chat_all_all(creator) VALUES (4)")
+conn.commit()
+
+c.execute("INSERT INTO chat_all_for_one(id_ch_c, id_user_add, time_add) VALUES (11, 10, '2024-05-18 11:17:32')")
+conn.commit()
+
+c.execute("INSERT INTO message_sskp(id_user, sms, type_sms, time) values (10, 'Сессия близко...', 'text', '2024-05-18 11:18:30')")
+conn.commit()
+
+c.execute("INSERT INTO chat(id_person_chat, id_stroke) values (2,3)")
 conn.commit()
 
 
-c.execute("INSERT INTO contact(id_user, id_contact) VALUES (1,4), (4,1)")
+# Пример корректного удаления данных из каждой таблицы
+c.execute("DELETE from user where id_user = 24")
 conn.commit()
 
 c.execute("DELETE from contact where id_user = 1 and id_contact = 4")
 conn.commit()
 
-
-c.execute("INSERT INTO chat_all_all(creator) VALUES (4)")
-conn.commit()
-
 c.execute("DELETE from chat_all_all where id_chat = 13")
-conn.commit()
-
-
-c.execute("INSERT INTO chat_all_for_one(id_ch_c, id_user_add, time_add) VALUES (11, 10, '2024-05-18 11:17:32')")
 conn.commit()
 
 c.execute("DELETE from chat_all_for_one where id_ch_c = 11 and id_user_add = 10")
 conn.commit()
 
-
-c.execute("INSERT INTO message_sskp(id_user, sms, type_sms, time) values (10, 'Сессия близко...', 'text', '2024-05-18 11:18:30')")
-conn.commit()
-
 c.execute("DELETE from message_sskp where id_stroke = 6")
-conn.commit()
-
-
-c.execute("INSERT INTO chat(id_person_chat, id_stroke) values (2,3)")
 conn.commit()
 
 c.execute("DELETE from chat where id_person_chat = 2 and id_stroke = 3")
 conn.commit()
 
 
+# Пример корректного изменения данных в таблице
 c.execute("UPDATE chat_all_all set (name, change_name) = ('GG', 1) where id_chat = 2")
 conn.commit()
-"""
+
 
 # Пример корректного изменения таблиц после удаления пользователя
 c.execute("INSERT INTO user (nickname, name, mail, password) VALUES ('turnoff', 'Виктор Тернов', 'turnoff@gmail.com', 'idktexttext')")
@@ -68,7 +66,7 @@ conn.commit()
 
 c.execute("DELETE from user where id_user = 24")
 conn.commit()
-#
+
 
 print('\nUser')
 c.execute('''Select * from user ''')
